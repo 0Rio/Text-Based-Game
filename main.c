ef440aaafd9d50bcf21dcf34afb27d2e2d5e2c81
Rio <100698972+0Rio@users.noreply.github.com>
2022-05-10 21:02:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 int main(void)
 {
-   
+   // \n for a new line 
+   printf(".   .  game name  .   .\n");
+   printf("\n\n");
+   printf("1. NEW GAME\n");
+   printf("2. CONTINUE");
+   printf("3. EXIT);
   
 }  
